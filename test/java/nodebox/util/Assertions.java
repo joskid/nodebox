f8441b28eac2cfe0adaf274830f9a941427bb7d7
Frederik De Bleser <frederik@burocrazy.com>
2012-02-02 15:23:40 +0100

    Remove unnecessary inputs in Assertions.

--- test/java/nodebox/util/Assertions.java
+++ test/java/nodebox/util/Assertions.java
@@ -1,25 +1,22 @@
 package nodebox.util;
 
 import com.google.common.collect.ImmutableList;
-import junit.framework.Assert;
 import nodebox.function.FunctionRepository;
 import nodebox.function.ListFunctions;
 import nodebox.function.MathFunctions;
 import nodebox.node.Node;
 import nodebox.node.NodeContext;
 
-import java.util.List;
-
 import static org.junit.Assert.assertEquals;
 
 public final class Assertions {
-    
+
     static private final FunctionRepository functionRepository = FunctionRepository.of(MathFunctions.LIBRARY, ListFunctions.LIBRARY);
 
     public static void assertResultsEqual(Iterable<?> result, Object... args) {
         assertEquals(ImmutableList.copyOf(args), ImmutableList.copyOf(result));
     }
-    
+
     public static void assertResultsEqual(Node network, Node child, Object... args) {
         NodeContext context = new NodeContext(functionRepository);
         Iterable<?> values = context.renderChild(network, child);
